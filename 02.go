335e1f45f70859a29ddfe6da8165410a5cddd8df
stmult <stmult@gmail.com>
2022-09-12 17:18:39 +0300

    02-massiv

--- 02.go
+++ 02.go
@@ -37,4 +37,80 @@ func main() {
 		83,
 	}
 	fmt.Println(x4[4])
+
+	slice1 := []int{1, 2, 3}
+	slice2 := append(slice1, 4, 5)
+	fmt.Println(slice1, slice2)
+
+	slice3 := []int{1, 2, 3}
+	slice4 := make([]int, 2)
+	copy(slice4, slice3)
+	fmt.Println(slice3, slice4)
+
+	map1 := make(map[string]int)
+	map1["key"] = 10
+	fmt.Println(map1["key"])
+
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Beryllium"
+	elements["B"] = "Boron"
+	elements["C"] = "Carbon"
+	elements["N"] = "Nitrogen"
+	elements["O"] = "Oxygen"
+	elements["F"] = "Fluorine"
+	elements["Ne"] = "Neon"
+	fmt.Println(elements["Li"])
+
+	name, ok := elements["Li"]
+	fmt.Println(name, ok)
+
+	elements2 := map[string]map[string]string{
+		"H": map[string]string{
+			"name":  "Hydrogen",
+			"state": "gas",
+		},
+		"He": map[string]string{
+			"name":  "Helium",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name":  "Lithium",
+			"state": "solid",
+		},
+		"Be": map[string]string{
+			"name":  "Beryllium",
+			"state": "solid",
+		},
+		"B": map[string]string{
+			"name":  "Boron",
+			"state": "solid",
+		},
+		"C": map[string]string{
+			"name":  "Carbon",
+			"state": "solid",
+		},
+		"N": map[string]string{
+			"name":  "Nitrogen",
+			"state": "gas",
+		},
+		"O": map[string]string{
+			"name":  "Oxygen",
+			"state": "gas",
+		},
+		"F": map[string]string{
+			"name":  "Fluorine",
+			"state": "gas",
+		},
+		"Ne": map[string]string{
+			"name":  "Neon",
+			"state": "gas",
+		},
+	}
+
+	if el, ok := elements2["Li"]; ok {
+		fmt.Println(el["name"], el["state"])
+	}
 }
