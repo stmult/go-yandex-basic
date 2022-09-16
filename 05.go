982f019f7bd6292ed723576d938cc813f36e07a5
stmult <stmult@gmail.com>
2022-09-16 12:23:10 +0300

    05-strukturi-interfaces

--- 05.go
+++ 05.go
@@ -5,24 +5,30 @@ import (
 	"math"
 )
 
+// создаем структуру
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+type Circle struct {
+	x1, y1, r1 float64
+}
+
+func (r *Rectangle) area() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return l * w
+}
+
 func distance(x1, y1, x2, y2 float64) float64 {
 	a := x2 - x1
 	b := y2 - y1
 	return math.Sqrt(a*a + b*b)
 }
-func rectangleArea(x1, y1, x2, y2 float64) float64 {
-	l := distance(x1, y1, x1, y2)
-	w := distance(x1, y1, x2, y1)
-	return l * w
-}
-func circleArea(x, y, r float64) float64 {
-	return math.Pi * r * r
-}
-func main() {
-	var rx1, ry1 float64 = 0, 0
-	var rx2, ry2 float64 = 10, 10
-	var cx, cy, cr float64 = 0, 0, 5
 
-	fmt.Println(rectangleArea(rx1, ry1, rx2, ry2))
-	fmt.Println(circleArea(cx, cy, cr))
+func main() {
+	r := Rectangle{0, 0, 10, 10}
+	c1 := Circle{0, 0, 5}
+	fmt.Println(r.area())
+	fmt.Println(c1)
 }
